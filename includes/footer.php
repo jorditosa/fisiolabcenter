c4b974c354998485cfda19fe3e95d93c0560cd70
jorditosa <73916178+jorditosa@users.noreply.github.com>
2022-02-20 00:22:59 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,47 @@
+<footer class="footer bg-dark border-top border-primary border-3 pt-4">
+    <div class="container-xl row mx-auto">
+        <div class="col-md-4 pb-4">
+            <h4 class="py-2 text-white border-bottom border-white">Contacta'ns</h4>
+            <div class="footer__contact pt-2">
+                <a class="d-flex py-1 text-decoration-none footer__contact__tel" href="tel:665091019">665 09 10 19</a>
+                <a class="d-flex py-1 text-decoration-none footer__contact__email" href="mailto:admin@fisiolabcenter.com">admin@fisiolabcenter.com</a>
+            </div>
+            <div class="footer__sociales text-center fs-4 py-3 d-flex justify-content-around">
+                <a href="https://www.facebook.com/fisiolabcenter/"><i class="bi bi-facebook footer__sociales--facebook"></i></a>
+                <a href="https://www.instagram.com/fisiolabcenter/?hl=es" class="ps-4"><i class="bi bi-instagram footer__sociales--instagram"></i></a>
+                <a class="qlwapp-toggle ps-4" data-action="open" data-phone="34665091019" data-message="¡Hola! Necessito demanar cita per..." href="https://web.whatsapp.com/send?phone=34665091019&amp;text=¡Hola! Necessito demanar cita per..." target="_blank">
+                    <i class="bi bi-whatsapp footer__sociales--whatsapp"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4 pb-4 mapa text-center">
+            <p class="m-0 d-block text-white py-2">Av Cubelles, 31 Vilanova i la Geltrú, BCN</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001.062301137445!2d1.7191816154206034!3d41.22041277927992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a386fbb4b71ff7%3A0xae5fe7c03b015e16!2sAv.%20de%20Cubelles%2C%2031%2C%2008800%20Vilanova%20i%20la%20Geltr%C3%BA%2C%20Barcelona!5e0!3m2!1ses!2ses!4v1644596527496!5m2!1ses!2ses" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+        </div>
+        <div class="col-md-4 pb-4">
+            <h4 class="py-2 text-white border-bottom border-white">Els nostres serveis</h4>
+            <div class="py-3">
+                <ul class="text-white">
+                    <li>Fisioteràpia</li>
+                    <li>Entrenament</li>
+                    <li>Ecografia</li>
+                    <li>Exercici terapèutic</li>
+                    <li>Biomecànica al running</li>
+                    <li>Pilates</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid border-top border-primary border-3"></div>
+    <div class="footer-copyright container-xl text-center d-flex justify-content-around">
+        <div class="col-md-4">
+            <a href="#">
+                <img src="build/img/Fisiolab-logo-blanco.webp" alt="imagen fisiolab blanca" class="logo__footer img-fluid py-3">
+            </a>
+        </div>
+        <div class="col-md-8 align-self-center">
+            <p class="m-0 text-white p-3 text-start">Fisiolab es el teu centre de salut referent al Garraf. &copy;Fisiolab Center 2022. Tots els drets reservats.</p>
+        </div>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Centre de salut a Vilanova i la Geltrú, Fisioteràpia">
+    <title>Fisiolab Center | Fisioteràpia, Nutrició, Podologia, Entrenament Vilanova i la Geltrú</title>
+
+    <link rel="shortcut icon" href="build/img/Fisiolablogo-IG-100.jpg" type="image/x-icon">
+    <link rel="icon" href="build/img/Fisiolablogo-IG-100.jpg" type="image/x-icon">
+    <link rel="canonical" href="https://www.fisiolabcenter.com/" />
+
+    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="build/css/animate.min.css">
+</head>
+
+<body class="fadein">
+    <header class="header container-fluid bg-dark border-3 border-primary border-top border-bottom ">
+        <nav class="row py-2 px-5 text-uppercase d-none d-md-flex">
+            <div class="col-md-5 navbar-nav flex-row justify-content-around px-1">
+                <a class="nav-link" href="equip.php">Equip</a>
+                <a class="nav-link" href="elcentre.php">El centre</a>
+                <a class="nav-link" href="blog.php">Blog</a>
+            </div>
+            <a href="index.php" class="col-md-2 logo navbar-brand flex-row justify-content-center mx-auto px-0">
+            </a>
+            <div class="col-md-5 navbar-nav flex-row justify-content-around px-1">
+                <a class="nav-link" href="serveis_fisioterapia.php">Serveis</a>
+                <a class="nav-link" href="entrenament.php">Fisiotraining</a>
+                <a class="nav-link" href="https://online.clinic-cloud.com/fisiolabcenter/" target="blank">Cita Online</a>
+            </div>
+        </nav>
+
+        <nav class="d-flex d-md-none py-2">
+            <a class="btn p-0 my-auto fs-1 col-3 d-flex justify-content-start" type="button" data-bs-toggle="offcanvas" data-bs-target="#menu">
+                <i class="bi-list text-primary"></i>
+            </a>
+            <div class="offcanvas offcanvas-start bg-primary" id="menu">
+                <div class="offcanvas-header py-4">
+                    <button class="btn-close" data-bs-dismiss="offcanvas"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div class="navbar-nav flex-column justify-content-around px-1 pb-3-offcanvas border-bottom">
+                        <a class="nav-link-offcanvas border-bottom" href="equip.php">Equip</a>
+                        <a class="nav-link-offcanvas border-bottom" href="elcentre.php">El centre</a>
+                        <a class="nav-link-offcanvas border-bottom" href="blog.php">Blog</a>
+                        <a class="nav-link-offcanvas border-bottom" href="serveis_fisioterapia.php">Serveis</a>
+                        <a class="nav-link-offcanvas border-bottom" href="contacte.php">Fisiotraining</a>
+                        <a class="nav-link-offcanvas" href="https://online.clinic-cloud.com/fisiolabcenter/" target="blank">Cita Online</a>
+                    </div>
+                    <div class="footer__sociales text-center fs-3 py-3 d-flex justify-content-evenly">
+                        <a href="https://www.facebook.com/fisiolabcenter/"><i class="bi bi-facebook"></i></a>
+                        <a href="https://www.instagram.com/fisiolabcenter/?hl=es"><i class="bi bi-instagram"></i></a>
+                        <a class="qlwapp-toggle" data-action="open" data-phone="34665091019" data-message="¡Hola! Necessito demanar cita per..." href="https://web.whatsapp.com/send?phone=34665091019&amp;text=¡Hola! Necessito demanar cita per..." target="_blank">
+                            <i class="bi bi-whatsapp"></i>
+                        </a>
+                    </div>
+                    <div class="navbar-nav px-1 pt-1 flex-column justify-content-around">
+                        <a class="text-dark text-center py-1 fs-6 text-decoration-none" href="mailto:admin@fisiolabcenter.com">admin@fisiolabcenter.com</a>
+                    </div>
+                </div>
+            </div>
+            <a href="index.php" class="col-5 logo navbar-brand flex-row justify-content-center mx-auto px-0">
+            </a>
+            <div class="col-4 text-end my-auto">
+                <a class="text-primary text-decoration-none text-uppercase fs-6" href="https://online.clinic-cloud.com/fisiolabcenter/" target="blank">Cita Online</a>
+            </div>
+        </nav>
+
+    </header>
